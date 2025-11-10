ffdb33f0045a86b19162f8b29a1171eae4fdf1e3
Antonina Danylchenko <antonina.ivanets@gmail.com>
2025-11-10 14:49:28 +0100

    Add solutions - create project, add models, serializers, and views to GET, POST, PUT, DELETE

---
+++ cinema/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Movie(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.CharField(max_length=225)
+    duration = models.IntegerField()
+
+    def __str__(self):
+        return f"Movie: {self.title}, duration: {self.duration}min"
